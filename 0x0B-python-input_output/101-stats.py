40e1acf0f3e47628a92f41ba27233fc36d9b7348
Luke Bamtefa <lukebamtefa@gmail.com>
2022-08-03 03:07:16 +0100

    advanced task 2

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -4,7 +4,7 @@
 After every ten lines or the input of a keyboard interruption (CTRL + C),
 prints the following statistics:
     - Total file size up to that point.
-    - Count of read status codes up to that point.
+    - Count of reead status codes up to that point.
 """
 
 
